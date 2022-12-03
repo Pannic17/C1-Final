d7069a9851888a14ee08a212d76ea300ebea0d39
pannic <pannic1984@outlook.com>
2022-12-03 14:20:31 +0000

    three init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,60 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import * as THREE from "three";
+import {onMounted} from "vue";
+import {GUI} from "three/examples/jsm/libs/lil-gui.module.min";
+
+
+import {setupThree} from "./three/setup";
+import {Lights} from "./three/lights";
+
+let scene, camera, renderer, gui;
+let control, composer, clock, light;
+
+let far = 1000;
+
+function initThree() {
+  setupThree();
+  let init = setupThree(far);
+  scene = init.scene;
+  camera = init.camera;
+  renderer = init.renderer;
+  control = init.control;
+  gui = new GUI();
+
+  let _gui = {
+    "Log": logCamera
+  }
+  gui.add(_gui, "Log");
+
+  function logCamera() {
+    console.log(camera);
+  }
+
+  clock = new THREE.Clock();
+  light = new Lights(scene, gui);
+
+  animate();
+}
+
+function animate () {
+  // composer.render();
+  renderer.render(scene,camera);
+  requestAnimationFrame(animate);
+}
+
+onMounted(() => {
+  initThree();
+  window.onresize = function (){
+    location.reload();
+  }
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="three-canvas"></div>
 </template>
 
 <style scoped>
