a2e7670e890b2d5ec1aa1c8f040bfc47e94d7a4e
pannic <pannic1984@outlook.com>
2022-12-04 22:05:10 +0000

    pixelate

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,7 @@ import {PaintShader} from "./three/shader/PaintShader";
 import {WaterShader} from "./three/shader/WaterShader";
 import {HalftoneEffect} from "./three/effect/p/HalftoneEffect";
 import {PostEffect} from "./three/effect/p/PostEffect";
+import {PixelEffect} from "./three/effect/p/PixelEffect";
 
 let scene, camera, renderer, gui;
 let control, composer, clock, light;
@@ -60,6 +61,7 @@ function initialize() {
   const effectLine = new LineEffect(composer, 100, innerWidth, innerHeight)
   const effectHalftone = new HalftoneEffect(composer, 100, innerWidth, innerHeight, Math.random()*6+6)
   const effectPost = new PostEffect(composer, 100, 20);
+  const effectPixel = new PixelEffect(composer, 50, scene, camera, 50)
 
   const effectTest1 = new ShaderPass( WaterShader );
   const effectTest2 = new ShaderPass( PaintShader );
@@ -82,7 +84,7 @@ function initialize() {
   document.onkeypress = function (e) {
     if (e.keyCode == 119) {
       console.log("PRESS w");
-      initEffect(effectPost);
+      initEffect(effectPixel);
     }
     if (e.keyCode == 115) {
       console.log("PRESS a");
---
+++ src/three/effect/p/PixelEffect.js
@@ -0,0 +1,41 @@
+import {RenderPixelatedPass} from "three/examples/jsm/postprocessing/RenderPixelatedPass";
+import {fade} from "../../utils";
+
+export class PixelEffect {
+    time = 0;
+    constructor (composer, max, scene, camera, fadeTP) {
+        this.composer = composer;
+        this.max = max;
+        // this.size = size;
+
+        const pixelPass = new RenderPixelatedPass(32, scene, camera);
+        this.pass = pixelPass;
+        this.fadeTP = fadeTP;
+    }
+
+    add() {
+        this.time = 0;
+        this.size = Math.random() * 16 + 32;
+        this.composer.addPass(this.pass);
+    }
+
+    end() {
+        this.composer.removePass(this.pass);
+    }
+
+    animate(onEnd) {
+        if (this.time == this.max) {
+            this.end();
+            this.time = 0;
+            onEnd();
+        } else {
+            if (this.time < this.fadeTP) {
+                let curve = fade(this.time/this.fadeTP) * this.fadeTP;
+                this.pass.pixelSize = Math.floor(curve);
+            } else if (this.time == this.fadeTP) {
+                this.pass.pixelSize = this.size;
+            }
+            this.time += 1;
+        }
+    }
+}
